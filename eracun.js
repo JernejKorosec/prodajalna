ee8c14d73b2ac0c58be0270876cfdcc177e92a5c
Jernej Korošec <jernejk.alfa@gmail.com>
2016-07-08 20:46:06 +0000

    Zaključek dela na I. Nalogi

--- eracun.js
+++ eracun.js
@@ -1,7 +1,7 @@
 //Priprava knjižnic
 var formidable = require("formidable");
 var util = require('util');
-
+var sporocilce = "";
 if (!process.env.PORT)
   process.env.PORT = 8080;
 
@@ -202,15 +202,40 @@ streznik.post('/prijava', function(zahteva, odgovor) {
   form.parse(zahteva, function (napaka1, polja, datoteke) {
     var napaka2 = false;
     try {
-      var stmt = pb.prepare("\
-        INSERT INTO Customer \
-    	  (FirstName, LastName, Company, \
-    	  Address, City, State, Country, PostalCode, \
-    	  Phone, Fax, Email, SupportRepId) \
-        VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
-      //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      var stmt = pb.prepare("" +
+        "INSERT INTO Customer" +
+    	  "(FirstName, LastName, Company, " +
+    	  "Address, City, State, Country, PostalCode, " +
+    	  "Phone, Fax, Email, SupportRepId) " +
+        "VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
+      
+      if(polja.FirstName.length == 0 || polja.LastName.length == 0 || polja.Email.length == 0  )
+        {
+          sporocilce = "Manjkajoči obvezni podatki o stranki. Prosimo vnesite vse obvezne podatke stranke.";
+          odgovor.redirect('/prijava');  
+        }
+        else{
+          try {
+          stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address,
+          polja.City, polja.State, polja.Country, polja.PostalCode, polja.Phone,
+          polja.Fax, polja.Email,3,
+            function(sqlErr) {
+                if(sqlErr) {
+                    sporocilce = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.";
+                    odgovor.redirect('/prijava');
+                } else {
+                  sporocilce = "Stranka je bila uspešno registrirana.";
+                }  
+            }
+          );
+          stmt.finalize();
+          odgovor.redirect('/prijava');
+          } catch (err) {
+            sporocilce = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.";
+            napaka2 = true;
+            console.log("Napaka: " + err);
+          }
+        }
     } catch (err) {
       napaka2 = true;
     }
@@ -223,7 +248,7 @@ streznik.post('/prijava', function(zahteva, odgovor) {
 streznik.get('/prijava', function(zahteva, odgovor) {
   vrniStranke(function(napaka1, stranke) {
       vrniRacune(function(napaka2, racuni) {
-        odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});  
+        odgovor.render('prijava', {sporocilo: sporocilce, seznamStrank: stranke, seznamRacunov: racuni});
       }) 
     });
 })
